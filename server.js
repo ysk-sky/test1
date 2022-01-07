3818091defc4dfc6a6d1b9bb0250dd9de5c667ce
ysks <clad.zero@gmail.com>
2022-01-07 17:25:10 +0900

    update app

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express")
+
+const app = express();
+const server = app.listen(process.env.PORT, function(){
+	console.log("Node.js is listening to PORT:"+server.address().port)
+});
+
+app.get("/", (req,res,next)=>{
+	res.status(200).sendFile(__dirname + '/index.html')
+})
